f357ee4260eee9f9fc5199b2e57b681340b1a025
Vendicated <vendicated@riseup.net>
2023-04-05 16:56:33 +0200

    forgor to add tsconfig

--- src/renderer/constants.ts
+++
@@ -1,1 +0,0 @@
-export const localStorage = window.localStorage;
--- src/renderer/fixes.ts
+++ src/renderer/fixes.ts
@@ -1,11 +1,11 @@
 import "./hideGarbage.css";
-import { isFirstRun } from "./utilts";
+import { isFirstRun } from "./utils";
 
 // Make clicking Notifications focus the window
 const originalSetOnClick = Object.getOwnPropertyDescriptor(Notification.prototype, "onclick")!.set!;
 Object.defineProperty(Notification.prototype, "onclick", {
     set(onClick) {
-        originalSetOnClick.call(this, function () {
+        originalSetOnClick.call(this, function (this: unknown) {
             onClick.apply(this, arguments);
             VencordDesktop.win.focus();
         });
@@ -13,6 +13,7 @@ Object.defineProperty(Notification.prototype, "onclick", {
     configurable: true
 });
 
+// Enable Desktop Notifications by default
 if (isFirstRun) {
     Vencord.Webpack.waitFor("setDesktopType", m => {
         m.setDesktopType("all");
