8f19d41d4c0859a8c2d21537c2d1ddf5a35c1925
Vendicated <vendicated@riseup.net>
2023-04-09 01:20:35 +0200

    Not infuriating at all

--- src/shared/IpcEvents.ts
+++ src/shared/IpcEvents.ts
@@ -3,7 +3,7 @@ export const GET_RENDERER_SCRIPT = "VCD_GET_RENDERER_SCRIPT";
 export const GET_RENDERER_STYLES = "VCD_GET_RENDERER_STYLES";
 
 export const RELAUNCH = "VCD_RELAUNCH";
-export const FOCUS = "VC_FOCUS";
+export const FOCUS = "VCD_FOCUS";
 
 export const SHOW_ITEM_IN_FOLDER = "VCD_SHOW_ITEM_IN_FOLDER";
 export const GET_SETTINGS = "VCD_GET_SETTINGS";
