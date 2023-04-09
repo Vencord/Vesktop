94b80ebe75b7b4cfc0820086f66013e59ca85ac7
Vendicated <vendicated@riseup.net>
2023-04-09 05:26:50 +0200

    Fix comment location

--- src/main/constants.ts
+++ src/main/constants.ts
@@ -2,12 +2,12 @@ import { app } from "electron";
 import { join } from "path";
 
 export const DATA_DIR = process.env.VENCORD_USER_DATA_DIR || join(app.getPath("userData"), "VencordDesktop");
-// needs to be inline require because of circular dependency
-// as otherwise "DATA_DIR" (which is used by ./settings) will be uninitialised
 export const VENCORD_SETTINGS_DIR = join(DATA_DIR, "settings");
 export const VENCORD_QUICKCSS_FILE = join(VENCORD_SETTINGS_DIR, "quickCss.css");
 export const VENCORD_SETTINGS_FILE = join(VENCORD_SETTINGS_DIR, "settings.json");
 
+// needs to be inline require because of circular dependency
+// as otherwise "DATA_DIR" (which is used by ./settings) will be uninitialised
 export const VENCORD_FILES_DIR = require("./settings").Settings.vencordDir || join(DATA_DIR, "vencordDist");
 
 export const USER_AGENT = `VencordDesktop/${app.getVersion()} (https://github.com/Vencord/Electron)`;
