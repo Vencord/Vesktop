07d647ba2e6da009b506851f3c9e7e20542d8128
Vendicated <vendicated@riseup.net>
2023-04-10 19:20:16 +0200

    Fix outdated code

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -9,4 +9,4 @@ import "./fixes";
 console.log("read if cute :3");
 
 export * as Components from "./components";
-export { PlainSettings, Settings } from "./settings";
+export { Settings } from "./settings";
