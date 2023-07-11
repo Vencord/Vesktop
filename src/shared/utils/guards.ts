72f83c3ac43c681f84fa793ac3aebcf93ec68c5e
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-07-11 19:35:15 +0000

    feat: add hide menu bar items on macOS (#50)
    Co-authored-by: V <vendicated@riseup.net>

---
+++ src/shared/utils/guards.ts
@@ -0,0 +1,13 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0
+ * Vencord Desktop, a desktop app aiming to give you a snappier Discord Experience
+ * Copyright (c) 2023 Vendicated and Vencord contributors
+ */
+
+export function isTruthy<T>(item: T): item is Exclude<T, 0 | "" | false | null | undefined> {
+    return Boolean(item);
+}
+
+export function isNonNullish<T>(item: T): item is Exclude<T, null | undefined> {
+    return item != null;
+}
