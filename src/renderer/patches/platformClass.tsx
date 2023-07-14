83dbc13bfba04f4f5c562780522f47bcfd1b2926
V <vendicated@riseup.net>
2023-07-14 02:06:30 +0200

    Address issues

--- src/renderer/patches/platformClass.tsx
+++ src/renderer/patches/platformClass.tsx
@@ -1,6 +1,6 @@
 /*
  * SPDX-License-Identifier: GPL-3.0
- * Vencord Desktop, a desktop app aiming to give you a snappier Discord Experience
+ * Vesktop, a desktop app aiming to give you a snappier Discord Experience
  * Copyright (c) 2023 Vendicated and Vencord contributors
  */
 
@@ -12,19 +12,11 @@ addPatch({
             find: "platform-web",
             replacement: {
                 // eslint-disable-next-line no-useless-escape
-                match: /return (?=__OVERLAY__\?""\.concat\((\i))/,
-                replace: "$1=$self.getPlatformClass(); return "
+                match: /(?<=" platform-overlay"\):)\i/,
+                replace: "$self.getPlatformClass()"
             }
         }
     ],
 
-    getPlatformClass() {
-        const platform = navigator.platform.toLowerCase();
-
-        if (platform.includes("mac")) return "platform-osx";
-        if (platform.includes("win")) return "platform-win";
-        if (platform.includes("linux")) return "platform-linux";
-
-        return "platform-web";
-    }
+    getPlatformClass: () => (navigator.platform.toLowerCase().startsWith("mac") ? "platform-osx" : "platform-web")
 });
