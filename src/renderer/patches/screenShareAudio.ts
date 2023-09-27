ba83bf6ff76b83c1e20fb576263aa11e55a3435a
Kaitlyn <87152313+kaitlynkittyy@users.noreply.github.com>
2023-09-26 23:10:36 -0400

    added platform checks and error handling when virtmic is not present

--- src/renderer/patches/screenShareAudio.ts
+++ src/renderer/patches/screenShareAudio.ts
@@ -1,36 +1,45 @@
-const original = navigator.mediaDevices.getDisplayMedia;
-async function getVirtmic() {
-    const getAudioDevice = async (deviceName: string|undefined) => {;
-        let devices = await navigator.mediaDevices.enumerateDevices();
-        let audioDevice = devices.find(({
-            label
-        }) => label === deviceName);
-    return audioDevice;
-    }    
+import { isLinux } from "../utils";
+if (isLinux) {
+    const original = navigator.mediaDevices.getDisplayMedia;
+    async function getVirtmic() {
+        const getAudioDevice = async (deviceName: string|undefined) => {;
+            let devices = await navigator.mediaDevices.enumerateDevices();
+            let audioDevice = devices.find(({
+                label
+            }) => label === deviceName);
+        return audioDevice;
+        }    
 
-    var id: string|undefined;
-    try {
-        let myDiscordAudioSink = await getAudioDevice('virtmic');
-        id = myDiscordAudioSink?.deviceId;
-    }
-    catch (error) {
-        id = 'default';
-    }
-return id;
+        var id: string|undefined;
+        try {
+            let myDiscordAudioSink = await getAudioDevice('virtmic');
+            id = myDiscordAudioSink?.deviceId;
+        }
+        catch (error) {
+            id = 'error';
+        }
+    return id;
+    };
+    navigator.mediaDevices.getDisplayMedia = async function() {
+        const id = await getVirtmic();
+        const stream = await original.apply(this);
+        if (id !== 'error'){
+            const audio = await navigator.mediaDevices.getUserMedia({ 
+                audio: { 
+                    deviceId: {
+                        exact: id
+                    },
+                    autoGainControl: false,
+                    echoCancellation: false,
+                    noiseSuppression: false
+                } 
+            });
+            audio.getAudioTracks().forEach(t => stream.addTrack(t));
+        } else {
+            const audio = await navigator.mediaDevices.getUserMedia({ audio: false });
+        
+        };
+        
+        return stream;
+    };
 };
-navigator.mediaDevices.getDisplayMedia = async function() {
-    const id = await getVirtmic();
-    const stream = await original.apply(this);
-
-  const audio = await navigator.mediaDevices.getUserMedia({ 
-    audio: { 
-        deviceId: {
-            exact: id
-        },
-        autoGainControl: false,
-        echoCancellation: false,
-        noiseSuppression: false
-   } });
-  audio.getAudioTracks().forEach(t => stream.addTrack(t));
-  return stream;
-}
