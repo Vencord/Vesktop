14b9b5fea73334d88f98a74fe83994abe5b7b1bd
V <vendicated@riseup.net>
2023-10-22 16:55:20 +0200

    Discard changes to src/main/firstLaunch.ts

--- src/main/firstLaunch.ts
+++ src/main/firstLaunch.ts
@@ -48,7 +48,6 @@ export function createFirstLaunchTour() {
         Settings.store.discordBranch = data.discordBranch;
         Settings.store.firstLaunch = false;
         Settings.store.arRPC = data.richPresence;
-        Settings.store.checkUpdates = true;
 
         if (data.autoStart) autoStart.enable();
 
