8eaa5206b98ce7029b6879cc2ce361c2ae5f6239
kaitlynkitty <87152313+kaitlynkittyy@users.noreply.github.com>
2024-04-17 19:40:03 -0400

    overhaul & improve Linux screenshare (#489)
    Fixes fps/resolution not properly being applied
    Enables hardware encoding via vaapi
    Redesigns stream picker modal 
    
    Co-authored-by: kaitlynkittyy <kaitlynyaadev@kaitlynyaa.dev>
    Co-authored-by: Oleh Polisan <polisanoleg@gmail.com>
    Co-authored-by: Vendicated <vendicated@riseup.net>

--- src/renderer/patches/screenShareFixes.ts
+++ src/renderer/patches/screenShareFixes.ts
@@ -4,7 +4,11 @@
  * Copyright (c) 2023 Vendicated and Vencord contributors
  */
 
+import { Logger } from "@vencord/types/utils";
+import { currentSettings } from "renderer/components/ScreenSharePicker";
 import { isLinux } from "renderer/utils";
+
+const logger = new Logger("VesktopStreamFixes");
 
 if (isLinux) {
     const original = navigator.mediaDevices.getDisplayMedia;
@@ -22,6 +26,29 @@ if (isLinux) {
     navigator.mediaDevices.getDisplayMedia = async function (opts) {
         const stream = await original.call(this, opts);
         const id = await getVirtmic();
+
+        const frameRate = Number(currentSettings?.fps);
+        const height = Number(currentSettings?.resolution);
+        const width = Math.round(height * (16 / 9));
+        const track = stream.getVideoTracks()[0];
+
+        track.contentHint = String(currentSettings?.contentHint);
+
+        const constraints = {
+            ...track.getConstraints(),
+            frameRate,
+            width: { min: 640, ideal: width, max: width },
+            height: { min: 480, ideal: height, max: height },
+            advanced: [{ width: width, height: height }],
+            resizeMode: "none"
+        };
+
+        track
+            .applyConstraints(constraints)
+            .then(() => {
+                logger.info("Applied constraints successfully. New constraints: ", track.getConstraints());
+            })
+            .catch(e => logger.error("Failed to apply constraints.", e));
 
         if (id) {
             const audio = await navigator.mediaDevices.getUserMedia({
