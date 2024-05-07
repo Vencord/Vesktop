d664a483bbd88fbb93ad9b70aa4dbf071c03d13e
MrGarlic <bsamans@samans.com>
2024-05-06 22:41:22 -0400

    added static icon link, improved tray settings css

--- src/renderer/components/settings/TrayIconImagePicker.tsx
+++ src/renderer/components/settings/TrayIconImagePicker.tsx
@@ -6,24 +6,24 @@
 
 import { Forms, Switch, Toasts } from "@vencord/types/webpack/common";
 import { Settings } from "renderer/settings";
-
+import { STATIC_DIR } from "shared/paths";
 import { SettingsComponent } from "./Settings";
 
 export const TrayIconImagePicker: SettingsComponent = ({ settings }) => {
     return (
         <>
-            <div id="tray-setting">
-                <div id="colLeft">
+            <div id="vcd-tray-setting">
+                <div className="vcd-tray-setting-switch">
                     <Switch
                         key="tray"
                         value={Settings.store.tray ?? false}
                         onChange={v => (Settings.store.tray = v)}
                         note={"Add a tray icon for Vesktop"}
                     >
-                        {"Tray Icon"}
+                        Tray Icon
                     </Switch>
                 </div>
-                <div id="colMiddle">
+                <div className="vcd-tray-setting-reset">
                     <Forms.FormText>
                         <a
                             href="about:blank"
@@ -36,36 +36,36 @@ export const TrayIconImagePicker: SettingsComponent = ({ settings }) => {
                         </a>
                     </Forms.FormText>
                 </div>
-                <div id="colRight">
-                    <div className="tray-icon-wrap">
-                        <img
-                            className="tray-icon-image"
-                            src={VesktopNative.tray.getTrayIcon()}
-                            alt="hello"
-                            width="48"
-                            height="48"
-                        ></img>
-                        <input
-                            className="edit-button"
-                            type="image"
-                            src="https://cdn.discordapp.com/attachments/895550066453012480/1236925384482619433/I1oxwE7.png?ex=6639c808&is=66387688&hm=5c5f47a0c06b2e0580fd5494386bfbeebc001c20e3fd64f26783f360b12162ed&"
-                            onClick={async () => {
-                                const choice = await VesktopNative.fileManager.selectTrayIcon();
-                                switch (choice) {
-                                    case "cancelled":
-                                        return;
-                                    case "invalid":
-                                        Toasts.show({
-                                            message: "Please select a valid .png or .jpg image!",
-                                            id: Toasts.genId(),
-                                            type: Toasts.Type.FAILURE
-                                        });
-                                        return;
-                                }
-                                settings.trayIconPath = choice;
-                            }}
-                        />
-                    </div>
+                <div className="vcd-tray-icon-wrap">
+                    <img
+                        className="vcd-tray-icon-image"
+                        src={VesktopNative.tray.getTrayIcon()}
+                        alt="hello"
+                        width="48"
+                        height="48"
+                    ></img>
+                    <input
+                        className="vcd-edit-button"
+                        type="image"
+                        src="https://raw.githubusercontent.com/Vencord/Vesktop/main/static/pencil-edit-icon.svg"
+                        width="40"
+                        height="40"
+                        onClick={async () => {
+                            const choice = await VesktopNative.fileManager.selectTrayIcon();
+                            switch (choice) {
+                                case "cancelled":
+                                    return;
+                                case "invalid":
+                                    Toasts.show({
+                                        message: "Please select a valid .png or .jpg image!",
+                                        id: Toasts.genId(),
+                                        type: Toasts.Type.FAILURE
+                                    });
+                                    return;
+                            }
+                            settings.trayIconPath = choice;
+                        }}
+                    />
                 </div>
             </div>
         </>
