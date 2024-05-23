f232cfc3c81e42fc054ca3f3fdd5a65166fc7692
Vendicated <vendicated@riseup.net>
2024-05-23 17:30:40 +0200

    Spellcheck: add language changer & swap position with paste/cut/copy

--- src/shared/settings.d.ts
+++ src/shared/settings.d.ts
@@ -21,8 +21,6 @@ export interface Settings {
     appBadge?: boolean;
     disableMinSize?: boolean;
     clickTrayToShowHide?: boolean;
-    /** @deprecated use customTitleBar */
-    discordWindowsTitleBar?: boolean;
     customTitleBar?: boolean;
 
     checkUpdates?: boolean;
@@ -30,6 +28,8 @@ export interface Settings {
     splashTheming?: boolean;
     splashColor?: string;
     splashBackground?: string;
+
+    spellCheckLanguages?: string[];
 }
 
 export interface State {
