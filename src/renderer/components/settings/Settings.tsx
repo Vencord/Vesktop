9515ab32ca72f82c5a93e86ae00dafb9191bf2ed
δDD <64313188+DDjivan@users.noreply.github.com>
2024-10-22 13:10:50 +0000

    Update Settings.tsx
    Fixed spelling mistake (Miscelleanous → Miscellaneous)

--- src/renderer/components/settings/Settings.tsx
+++ src/renderer/components/settings/Settings.tsx
@@ -103,7 +103,7 @@ const SettingsOptions: Record<string, Array<BooleanSetting | SettingsComponent>>
         }
     ],
     Notifications: [NotificationBadgeToggle],
-    Miscelleanous: [
+    Miscellaneous: [
         {
             key: "arRPC",
             title: "Rich Presence",
