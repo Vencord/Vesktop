4ee57da6f3b516e57a9a39388f2390be1ac63bf9
Vendicated <vendicated@riseup.net>
2025-02-06 03:47:34 +0100

    remove legacy migration code

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -14,8 +14,6 @@ import "./arrpc";
 console.log("read if cute :3");
 
 export * as Components from "./components";
-import { onceReady } from "@vencord/types/webpack";
-import { Alerts } from "@vencord/types/webpack/common";
 
 import SettingsUi from "./components/settings/Settings";
 import { Settings } from "./settings";
@@ -31,19 +29,3 @@ customSettingsSections.push(() => ({
     element: SettingsUi,
     className: "vc-vesktop-settings"
 }));
-
-// TODO: remove soon
-const vencordDir = "vencordDir" as keyof typeof Settings.store;
-if (Settings.store[vencordDir]) {
-    onceReady.then(() =>
-        setTimeout(
-            () =>
-                Alerts.show({
-                    title: "Custom Vencord Location",
-                    body: "Due to security hardening changes in Vesktop, your custom Vencord location had to be reset. Please configure it again in the settings.",
-                    onConfirm: () => delete Settings.store[vencordDir]
-                }),
-            5000
-        )
-    );
-}
