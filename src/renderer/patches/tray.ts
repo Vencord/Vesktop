89a88f7bcd8b8d4233843a0cb796ea30244ae98b
Oleh Polisan <polisanoleg@gmail.com>
2025-02-15 12:29:25 +0200

    headers fixes

--- src/renderer/patches/tray.ts
+++ src/renderer/patches/tray.ts
@@ -1,7 +1,7 @@
 /*
- * SPDX-License-Identifier: GPL-3.0
  * Vesktop, a desktop app aiming to give you a snappier Discord Experience
- * Copyright (c) 2023 Vendicated and Vencord contributors
+ * Copyright (c) 2025 Vendicated and Vesktop contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
 import { Logger } from "@vencord/types/utils";
