a93b82f14b33aff023616dbd95c72330ec717bf6
Tiagoquix <30274161+Tiagoquix@users.noreply.github.com>
2025-04-14 00:09:03 -0300

    Commit suggestions

--- src/main/index.ts
+++ src/main/index.ts
@@ -69,14 +69,21 @@ function init() {
     // WidgetLayering (Vencord Added): Fix DevTools context menus https://github.com/electron/electron/issues/38790
     disabledFeatures.push("WinRetrieveSuggestionsOnlyOnDemand", "HardwareMediaKeyHandling", "MediaSessionService");
 
-    // FIXME: Remove this when it's fixed upstream
-    // Fix weird font rendering + fix blurry font on Linux
+    // FIXME: Remove when bumping to Electron >36
+    // Obviously, check if this has been fixed first
+    // before removing the flag.
+    //
+    // This fixes weird font rendering,
+    // including blurry fonts.
+    //
+    // Not limiting to any specific OS
+    // because there are complaints from users
+    // from all relevant OSes.
+    //
     // https://github.com/googlefonts/fontations/issues/1359
     // https://issues.chromium.org/issues/375219950
     // https://issues.skia.org/issues/396360753
-    if (process.platform === "linux") {
-        disabledFeatures.push("FontationsFontBackend");
-    }
+    disabledFeatures.push("FontationsFontBackend");
 
     // Support TTS on Linux using speech-dispatcher
     app.commandLine.appendSwitch("enable-speech-dispatcher");
