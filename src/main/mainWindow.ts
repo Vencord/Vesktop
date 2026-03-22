3a9cfe9e2838c6c27f6c85b16ac68583109490a2
V <vendicated@riseup.net>
2026-03-22 19:18:14 +0100

    undo licence change

--- src/main/mainWindow.ts
+++ src/main/mainWindow.ts
@@ -1,6 +1,6 @@
 /*
  * Vesktop, a desktop app aiming to give you a snappier Discord Experience
- * Copyright (c) 2026 Vendicated and Vesktop contributors
+ * Copyright (c) 2023 Vendicated and Vencord contributors
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
