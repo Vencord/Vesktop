dff16950acbdc9f7d45b47fb2a9b8413c10c4b8f
V <vendicated@riseup.net>
2026-07-21 03:05:03 +0200

    Add error boundary to WebRTC IP Handling Policy picker
    Wrap WebRTC IP Handling Policy picker in SimpleErrorBoundary and update help text.

--- src/renderer/components/settings/WebRTCIPHandlingPolicyPicker.tsx
+++ src/renderer/components/settings/WebRTCIPHandlingPolicyPicker.tsx
@@ -7,26 +7,36 @@
 import { Heading, Margins, Paragraph } from "@vencord/types/components";
 import { Select } from "@vencord/types/webpack/common";
 
+import { SimpleErrorBoundary } from "../SimpleErrorBoundary";
 import { SettingsComponent } from "./Settings";
 
 export const WebRTCIPHandlingPolicyPicker: SettingsComponent = ({ settings }) => {
     return (
-        <div>
-            <Heading tag="h5">WebRTC IP Handling Policy</Heading>
-            <Paragraph className={Margins.bottom8}>Specify the WebRTC IP handling policy.</Paragraph>
-            <Select
-                placeholder="Default"
-                options={[
-                    { label: "Default", value: "default", default: true },
-                    { label: "Default Public Interface Only", value: "default_public_interface_only" },
-                    { label: "Default Public And Private Interfaces", value: "default_public_and_private_interfaces" },
-                    { label: "Disable Non-Proxied UDP", value: "disable_non_proxied_udp" }
-                ]}
-                closeOnSelect={true}
-                select={v => (settings.webRTCIPHandlingPolicy = v)}
-                isSelected={v => v === settings.webRTCIPHandlingPolicy}
-                serialize={s => s}
-            />
-        </div>
+        <SimpleErrorBoundary>
+            <div>
+                <Heading tag="h5">WebRTC IP Handling Policy</Heading>
+                <Paragraph className={Margins.bottom8}>
+                    Changing this may help with voice connection issues on some networks, most notably VPNs like
+                    Tailscale.
+                </Paragraph>
+                <Select
+                    placeholder="Default"
+                    options={[
+                        { label: "Default", value: "default", default: true },
+                        { label: "Default Public Interface Only", value: "default_public_interface_only" },
+
+                        {
+                            label: "Default Public And Private Interfaces",
+                            value: "default_public_and_private_interfaces"
+                        },
+                        { label: "Disable Non-Proxied UDP", value: "disable_non_proxied_udp" }
+                    ]}
+                    closeOnSelect={true}
+                    select={v => (settings.webRTCIPHandlingPolicy = v)}
+                    isSelected={v => v === (settings.webRTCIPHandlingPolicy ?? "default")}
+                    serialize={s => s}
+                />
+            </div>
+        </SimpleErrorBoundary>
     );
 };
